bf28cd54d4298ed820bbd936061f52c343351953
Claus Wahlers <claus@codeazur.com.br>
2021-02-07 11:20:57 -0300

    Remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import React, { useRef, useState, useEffect, useCallback } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { formatDistance, lightFormat } from 'date-fns';
-import { loadDigest, formatNumber } from 'utils';
+import { lightFormat } from 'date-fns';
+import { loadDigest } from 'utils';
 
 import File from 'components/File';
 
