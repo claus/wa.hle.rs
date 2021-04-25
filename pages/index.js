c31a63a615260bd87b8fbb228242d015f4b8460d
Claus Wahlers <claus@codeazur.com.br>
2021-04-25 00:05:23 -0300

    Initial b17 commit

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,87 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
-import { lightFormat } from 'date-fns';
-import { loadDigest } from 'utils';
 
-import SectionChannel from 'components/SectionChannel';
-import SectionWebsite from 'components/SectionWebsite';
+import { vigeneredHexSolver } from 'utils';
+
+import Head from 'next/head';
 
 import styles from './index.module.scss';
 
-const LOAD_INTERVAL = 15 * 60 * 1000;
-
-const Landing = ({ digest: initialDigest, channel: initialChannel }) => {
-    const loadInterval = useRef();
-    const updateInterval = useRef();
-    const [lastLoad, setLastLoad] = useState(Date.now());
-    const [countdownLoad, setCountdownLoad] = useState('Updating in');
-    const [isUpdating, setIsUpdating] = useState(false);
-    const [forceUpdate, setForceUpdate] = useState(0);
-    const [digest, setDigest] = useState(initialDigest)
-    const [channel, setChannel] = useState(initialChannel)
-
-    const update = async () => {
-        setIsUpdating(true);
-        const { digest, channel } = await loadDigest();
-        setDigest(digest);
-        setChannel(channel);
-        setLastLoad(Date.now());
-        setIsUpdating(false);
-    }
-
-    useEffect(() => {
-        setLastLoad(Date.now());
-        setCountdownLoad('Updating in');
-        loadInterval.current = setInterval(update, LOAD_INTERVAL);
-        return () => {
-            clearInterval(loadInterval.current);
-        }
-    }, [forceUpdate]);
-
-    useEffect(() => {
-        const setCountdownMessage = () => {
-            const msLeft = Math.max(-(Date.now() - lastLoad - LOAD_INTERVAL), 0);
-            const distance = lightFormat(new Date(msLeft), 'm:ss');
-            setCountdownLoad(`Updating in ${distance}`);
-        }
-        setCountdownMessage();
-        updateInterval.current = setInterval(setCountdownMessage, 1000);
-        return () => {
-            clearInterval(updateInterval.current);
-        }
-    }, [lastLoad]);
-
-    const handleUpdateNowClick = () => {
-        update();
-        setForceUpdate(forceUpdate + 1);
-    }
-
-    const countdownMessage = isUpdating ? 'Updating now...' : countdownLoad;
-
-    const updateButtonClass = cx(styles.updateButton, {
-        [styles.disabled]: isUpdating,
-    });
-
+const Landing = () => {
+    const input = useRef();
+    const [results, setResults] = useState([]);
+    const handleSubmit = event => {
+        try {
+            setResults(vigeneredHexSolver(input.current.value));
+        } catch (e) {}
+        event.preventDefault();
+    };
     return (
-        <main>
-            <h1>Father Archive</h1>
-            <div className={styles.countdownMessage}>{countdownMessage}</div>
-            <button className={updateButtonClass} onClick={handleUpdateNowClick}>Update now</button>
-            <SectionChannel channel={channel} />
-            <SectionWebsite digest={digest} />
-        </main>
+        <div className={styles.container}>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <title>∩∩∩∩∩∩∩∩∩∩∩∩∩</title>
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>∩</h1>
+                <form onSubmit={handleSubmit}>
+                    <fieldset className={styles.fieldset}>
+                        <legend>
+                            Please enter Vigenère-encrypted hex code
+                        </legend>
+                        <div className={styles.inputs}>
+                            <textarea
+                                name="input"
+                                ref={input}
+                                className={styles.input}
+                                defaultValue="64 48 64 6g 61 47 56 35 4c 33 5h 6q 64 47 39 6g 59 32 68 76 64 57 4g 6m 5o 57 52 33 5e 58 52 6h 5h 58 4s 6j 62 32 56 6c 61 47 39 79 5h 57 35 76 64 32 56 33 64 47 68 6q 63 32 56 6e 61 47 39 6h 62 33 4h 6q 63 6i 64 6e 64 57 35 7h 64 47 56 6o 61 47 39 6j 63 6f 39 31 5h 32 68 30 61 47 56 74"
+                            />
+                            <input
+                                type="submit"
+                                name="submit"
+                                value="Submit"
+                                className={styles.submit}
+                            />
+                        </div>
+                    </fieldset>
+                </form>
+                <div className={styles.results}>
+                    {results.map((result, i) => (
+                        <Result key={i} data={result} />
+                    ))}
+                </div>
+            </main>
+        </div>
     );
 };
 
-export async function getServerSideProps() {
-    const initialDigest = await loadDigest();
-    return {
-        props: { ...initialDigest },
-    };
-}
+const Result = ({ data }) => {
+    const rows = data.reduce((a, letters) => Math.max(a, letters.length), 0);
+    return (
+        <div className={styles.result}>
+            <h2 className={styles.headline}>{data.length} letter keys</h2>
+            <table className={styles.table}>
+                {new Array(rows).fill(0).map((_, i) => (
+                    <tr key={i} className={styles.tr}>
+                        {data.map((letters, j) => (
+                            <td key={j} className={styles.td}>
+                                {letters[i] ?? ''}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </table>
+        </div>
+    );
+};
 
-Landing.propTypes = {
-    digest: PropTypes.array.isRequired,
-    channel: PropTypes.array.isRequired,
-}
+Result.propTypes = {
+    data: PropTypes.array.isRequired,
+};
 
 export default Landing;
--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,70 @@
-import axios from 'axios';
+function modulo(a, b) {
+    return ((a % b) + b) % b;
+}
 
-export async function loadDigest() {
-    const response = await axios.get('https://father.codeazur.com.br/website.json');
-    const digest = Object.values(response.data)
-        .sort((a, b) => {
-            const aDate = new Date(a.time).getTime();
-            const bDate = new Date(b.time).getTime();
-            return bDate - aDate;
-        });
+export function vigeneredHexSolver(cipher) {
+    const letters = cipher
+        .toLowerCase()
+        .split('')
+        .filter(c => c >= 'a' && c <= 'z');
 
-    const channel = await axios.get('https://father.codeazur.com.br/channel.json');
+    if (letters.length === 0) {
+        return [];
+    }
 
-    return { digest, channel: channel.data };
+    const letterCodes = letters.map(c => c.charCodeAt(0) - 97);
+
+    const combinations = letterCodes.map(letter =>
+        new Array(6)
+            .fill(0)
+            .map((_, i) => String.fromCharCode(modulo(letter - i, 26) + 97))
+    );
+
+    const combinationKeyLens = combinations.map((combination, i) => {
+        return combination
+            .map(c => {
+                const keylens = [];
+                for (let keylen = 2; keylen <= 13; keylen++) {
+                    if (keylen + i >= combinations.length) {
+                        continue;
+                    }
+                    let possible = true;
+                    for (
+                        let j = keylen + i;
+                        j < combinations.length;
+                        j += keylen
+                    ) {
+                        if (!combinations[j].includes(c)) {
+                            possible = false;
+                            break;
+                        }
+                    }
+                    if (possible) {
+                        keylens.push(keylen);
+                    }
+                }
+                return { letter: c, keylens };
+            })
+            .filter(({ keylens }) => keylens.length > 0);
+    });
+
+    const potentialSolutions = [];
+    for (let keylen = 2; keylen <= 13; keylen++) {
+        const wordCombinations = [];
+        for (let i = 0; i < keylen; i++) {
+            const combinations = combinationKeyLens[i] || [];
+            const letters = [];
+            wordCombinations.push(letters);
+            combinations.forEach(({ letter, keylens }) => {
+                if (keylens.includes(keylen)) {
+                    letters.push(letter);
+                }
+            });
+        }
+        if (!wordCombinations.find(letters => letters.length === 0)) {
+            potentialSolutions.push(wordCombinations);
+        }
+    }
+
+    return potentialSolutions;
 }
