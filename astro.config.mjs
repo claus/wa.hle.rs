def2383a8148ebc753300189b3fe3804ea092b0b
Claus Wahlers <claus@codeazur.com.br>
2024-12-20 23:18:25 -0300

    Update to Astro 5, remove Volume font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
+
+const isNodeAdapter = process.argv.includes('--node');
+const adapter = isNodeAdapter ? node({ mode: 'standalone' }) : vercel();
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://wa.hle.rs',
     integrations: [sitemap(), mdx()],
-    output: 'hybrid',
-    adapter: vercel(),
+    output: 'static',
+    adapter,
 });
