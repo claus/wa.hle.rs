34516f2652d3b8df7fdae5541015a765a2e655fe
Claus Wahlers <claus@codeazur.com.br>
2026-01-16 23:55:57 -0300

    Add HelVF font

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,25 @@ const adapter = isNodeAdapter ? node({ mode: 'standalone' }) : vercel();
 // https://astro.build/config
 export default defineConfig({
     site: 'https://wa.hle.rs',
+    devToolbar: { enabled: false },
     integrations: [sitemap(), mdx()],
     output: 'static',
     adapter,
+    experimental: {
+        fonts: [
+            {
+                provider: 'local',
+                name: 'Hel',
+                cssVariable: '--font-hel',
+                fallbacks: ['Arial', 'sans-serif'],
+                variants: [
+                    {
+                        weight: '400',
+                        style: 'normal',
+                        src: ['./src/assets/fonts/HelVF.woff2'],
+                    },
+                ],
+            },
+        ],
+    },
 });
